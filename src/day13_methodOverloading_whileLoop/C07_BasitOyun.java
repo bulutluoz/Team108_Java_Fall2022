739698a9592542d06c79c6b48538f733a993683f
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-12-05 10:39:30 +0100

    14.gun sonu

--- src/day13_methodOverloading_whileLoop/C07_BasitOyun.java
+++ src/day13_methodOverloading_whileLoop/C07_BasitOyun.java
@@ -1,6 +1,7 @@
 package day13_methodOverloading_whileLoop;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class C07_BasitOyun {
 
@@ -9,7 +10,7 @@ public class C07_BasitOyun {
             random olarak 1 ile 100 arasinda bir sayi olusturun
             kullanicidan bu sayiyi bilmesini isteyin
             kullanici her deger girdiginde
-            tuttugumuz sayi ile karsilastirip tahminini buyult/kucult diyelim
+            tuttugumuz sayi ile karsilastirip tahminini buyut/kucult diyelim
             kullanici tuttugumuz sayiyi bildiginde
             tahmin sayisini ve
 
@@ -21,6 +22,35 @@ public class C07_BasitOyun {
 
         Random rnd= new Random();
         int tutulanSayi= rnd.nextInt(100);
+        int sayac=1;
+        int tahmin=0;
+
+        Scanner scan = new Scanner(System.in);
+
+        while(tutulanSayi != tahmin){
+
+            System.out.println("Lutfen bir tahmin giriniz");
+            tahmin= scan.nextInt();
+
+            if (tutulanSayi==tahmin){ // sayiyi bulduysa
+                System.out.println(sayac +" tahmin ile tuttugum sayiyi buldun");
+                if (sayac<=3){
+                    System.out.println("Vaowww");
+                } else if (sayac<=8) {
+                    System.out.println("Aferin");
+                }else {
+                    System.out.println("Basarisiz");
+                }
+                break;
+            } else if (tahmin<tutulanSayi) {
+                System.out.println("Sayiyi buyut");
+                sayac++;
+            }else {
+                System.out.println("Sayiyi kucult");
+                sayac++;
+            }
+        }
+
 
 
     }
