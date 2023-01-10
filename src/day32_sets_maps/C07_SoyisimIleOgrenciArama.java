744b1509194786916e0f948c47ea96b1dc8a633e
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-01-10 12:08:15 +0100

    32.gun sonu

--- src/day32_sets_maps/C07_SoyisimIleOgrenciArama.java
+++ src/day32_sets_maps/C07_SoyisimIleOgrenciArama.java
@@ -1,7 +1,18 @@
 package day32_sets_maps;
 
+import java.util.Map;
+
 public class C07_SoyisimIleOgrenciArama {
 
     // ogrenci map'de aradigimiz soyisim'deki ogrencilerin
-    // value'lerini yazdiran bir method olusturun
+    // Isim,soyisim sinif ve sube bilgilerini
+    // bir tablo olarak yazdiran bir method olusturun
+
+    public static void main(String[] args) {
+
+        Map<Integer,String> ogrenciMap = MapMethodDepo.ogrenciMapOlustur();
+
+        MapMethodDepo.soyisimIleOgrenciListesiYazdirma(ogrenciMap,"Cem");
+
+    }
 }
--- src/day32_sets_maps/MapMethodDepo.java
+++ src/day32_sets_maps/MapMethodDepo.java
@@ -49,4 +49,36 @@ public class MapMethodDepo {
         return false;
 
     }
+
+    public static void soyisimIleOgrenciListesiYazdirma(Map<Integer, String> ogrenciMap, String soyisim) {
+
+        // 1- map'deki value'leri bir collection olarak kaydedelim
+        //    [Ali-Can-11-H-MF, Veli-Cem-10-K-TM, Ali-Cem-11-K-TM, Ayse-Can-10-H-MF, Sevgi-Cem-11-M-TM, Sevgi-Can-10-K-MF]
+        Collection<String> ogrenciValueCollection=ogrenciMap.values();
+        // listenin ilk satiri olarak basliklari yazdiralim
+        System.out.println("Isim  Soyisim  Sinif Sube");
+
+        // 2- for-each ile her bir value bilgisini ele alalim Ali-Can-11-H-MF
+        // 3- value'yu - ile split yapip bir array elde edelim [Ali, Can, 11, H, MF]
+        // 4- array'in 1.index'indeki soyisme bakip, aranan soyisim ile ayni ise
+        //    isim, soyisim, sinif ve sube bilgilerini yazdiralim
+
+        for (String eachValue: ogrenciValueCollection
+             ) {
+            String[] valueArr=eachValue.split("-");
+            String valueSoyisim= valueArr[1];
+
+            if (valueSoyisim.equalsIgnoreCase(soyisim)){
+                System.out.println(valueArr[0] + "  "+
+                                    valueArr[1]+ "  "+
+                                    valueArr[2]+"  "+
+                                    valueArr[3]);
+            }
+
+        }
+
+
+
+
+    }
 }
